5713ba319c0124cd979266051f21171d09fa552c
olaf86 <yogawa0417@gmail.com>
2026-05-31 14:35:39 +0900

    Limit CI editmode tests to Odoro assembly

---
+++ Assets/Odoro/Tests/EditMode/CiSmokeTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Odoro.Tests
+{
+    public sealed class CiSmokeTests
+    {
+        [Test]
+        public void EditModeTestAssemblyIsDiscoverable()
+        {
+            Assert.Pass();
+        }
+    }
+}
